fff7fcdab34d343731ad853078f1fec03af03373
Mica <mika89700@gmail.com>
2021-02-17 12:55:21 +0100

    uncommente class main

--- problem_set/simple_circuits_20_template.py
+++ problem_set/simple_circuits_20_template.py
@@ -38,21 +38,19 @@ def simple_circuits_20(angle):
     return prob
 
 
-# if __name__ == "__main__":
-#     # DO NOT MODIFY anything in this code block
-#
-#     # Load and process input
-#     angle_str = sys.stdin.read()
-#     angle = float(angle_str)
-#
-#     ans = simple_circuits_20(angle)
-#
-#     if isinstance(ans, np.tensor):
-#         ans = ans.item()
-#
-#     if not isinstance(ans, float):
-#         raise TypeError("the simple_circuits_20 function needs to return a float")
-#
-#     print(ans)
+if __name__ == "__main__":
+    # DO NOT MODIFY anything in this code block
 
-print(simple_circuits_20(0.9572))
+    # Load and process input
+    angle_str = sys.stdin.read()
+    angle = float(angle_str)
+
+    ans = simple_circuits_20(angle)
+
+    if isinstance(ans, np.tensor):
+        ans = ans.item()
+
+    if not isinstance(ans, float):
+        raise TypeError("the simple_circuits_20 function needs to return a float")
+
+    print(ans)
