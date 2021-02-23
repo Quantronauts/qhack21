0619eca8da0add2583228763383268cf76964ef7
Mica <mika89700@gmail.com>
2021-02-23 13:08:13 +0100

    multi sensor

---
+++ hackaton/sensors/u2_u3_sensor.py
@@ -0,0 +1,26 @@
+######################################
+# Simple sensor using RX and RZ gates
+######################################
+
+import pennylane as qml
+
+
+def sensor(datas):
+    qml.RX(datas[0], wires=0)
+    if len(datas) == 3:
+        qml.RY(datas[1], wires=0)
+        qml.RZ(datas[2], wires=0)
+    else:
+        qml.RZ(datas[1], wires=0)
+    return qml.probs(0)
+
+
+##############################################################################
+# Testing
+
+# dev = qml.device('default.qubit', wires=1)
+# toto = [2.14, 3.14]
+# circ = qml.QNode(sensor, dev)
+# circ(toto)
+# print(circ.draw())
+
