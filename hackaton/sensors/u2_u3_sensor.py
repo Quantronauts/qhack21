313e3de30469b038705b3fd9ccf3794c477f540e
Mica <mika89700@gmail.com>
2021-02-23 13:47:31 +0100

    typo

--- hackaton/sensors/u2_u3_sensor.py
+++ hackaton/sensors/u2_u3_sensor.py
@@ -7,12 +7,12 @@ import pennylane as qml
 
 @qml.template
 def sensor(datas, wires):
-    qml.RX(datas[0], wires[0])
+    qml.RX(datas[0], wires=wires[0])
     if len(datas) == 3:
-        qml.RY(datas[1], wires[0])
-        qml.RZ(datas[2], wires[0])
+        qml.RY(datas[1], wires=wires[0])
+        qml.RZ(datas[2], wires=wires[0])
     else:
-        qml.RZ(datas[1], wires[0])
+        qml.RZ(datas[1], wires=wires[0])
     #return qml.probs(0)
 
 
