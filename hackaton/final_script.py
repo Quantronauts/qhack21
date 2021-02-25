3381003ad05d9d25523c3d81b9813393c227a394
Mica <mika89700@gmail.com>
2021-02-25 15:08:23 +0100

    generative script file

--- hackaton/final_script.py
+++ hackaton/final_script.py
@@ -5,6 +5,7 @@
 import pennylane as qml
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import sys
 
 from sensors.u2_u3_sensor import sensor
@@ -28,15 +29,21 @@ print("##################################################################\n"
     Variables :
     data_training : the xy // xyz data for only 1 qubit
     label_training : the label for only 1 qubit
-    theta : the angle for the classifier
+    n_layers : deep level
+    weights : the angle for the classifier
 """
 
-dev = qml.device('default.qubit', wires=1, shots=3)
+dev = qml.device('default.qubit', wires=1, shots=1)
 n_qubits = len(dev.wires)
 n_layers = 2
 weights = np.random.uniform(low=-np.pi, high=np.pi, size=(n_layers * n_qubits, 3))
 
-#for i in range(len(csv_training[0])):
+if csv_training.shape[1] == 3:
+    prediction = [[], [], []]
+else:
+    prediction = [[], [], [], []]
+
+# for i in range(len(csv_training[0])):
 for i in range(20):
     if csv_training.shape[1] == 3:
         data_training = [csv_training[0][i], csv_training[1][i]]
@@ -45,8 +52,6 @@ for i in range(20):
         data_training = [csv_training[0][i], csv_training[1][i], csv_training[2][i]]
         label_training = csv_training[3][i]
 
-    # dev = qml.device('default.qubit', wires=1, shots=3)
-
 
     @qml.qnode(dev)
     def circuit(datas, params):
@@ -54,9 +59,31 @@ for i in range(20):
         classifier(params, wires=dev.wires)
         return qml.sample(qml.PauliZ(0))
 
+
     result = circuit(data_training, weights)
     prob = result
-    print("Data : {} ; {} = {}".format(data_training[0], data_training[1], prob))
+
+    # Graph and check error
+    prediction[0].append(data_training[0])
+    prediction[1].append(data_training[1])
+    if csv_training.shape[1] == 3:
+        prediction[2].append(prob)
+        print("Data : {} ; {} = {}".format(data_training[0], data_training[1], prob))
+    else:
+        prediction[2].append(data_training[2])
+        prediction[3].append(prob)
+        print("Data : {} ; {} ; {} = {}".format(data_training[0], data_training[1], data_training[1], prob))
+
+print("\nTraining done !")
+
+if len(prediction) == 3:
+    plt.scatter(prediction[1], prediction[0], c=prediction[2], cmap="coolwarm")
+    plt.plot([0,2*np.pi], [np.pi/2, np.pi/2], linestyle='--', c='#000000')
+    plt.colorbar()
+else:
+    fig = plt.figure().gca(projection='3d')
+    fig.scatter(prediction[0], prediction[1], prediction[2], c=prediction[3], cmap="coolwarm")
+plt.show()
 
 
 
