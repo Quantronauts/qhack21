2fca9ca36f9c07221da9ce75f307296f0768946c
Mica <mika89700@gmail.com>
2021-02-25 21:48:35 +0100

    typo

--- hackaton/final_script.py
+++ hackaton/final_script.py
@@ -20,9 +20,6 @@ from classifiers.simple_1rot_classifier import classifier
 csv_training = pd.read_csv('datas/training_data_xz.csv', header=None)
 csv_testing = pd.read_csv('datas/testing_data_xz.csv', header=None)
 
-##################################################################
-# Training phase
-
 print("##################################################################\n"
       "# TRAINING")
 """
@@ -31,12 +28,16 @@ print("##################################################################\n"
     label_training : the label for only 1 qubit
     n_layers : deep level
     weights : the angle for the classifier
+    wires : number of qubits for the classifier
+    shots : number of shots
 """
 
 dev = qml.device('default.qubit', wires=1, shots=1)
 n_qubits = len(dev.wires)
 n_layers = 2
 weights = np.random.uniform(low=-np.pi, high=np.pi, size=(n_layers * n_qubits, 3))
+opt = qml.GradientDescentOptimizer(stepsize=0.1)
+error_rate = 0
 
 if csv_training.shape[1] == 3:
     prediction = [[], [], []]
@@ -44,7 +45,7 @@ else:
     prediction = [[], [], [], []]
 
 # for i in range(len(csv_training[0])):
-for i in range(20):
+for i in range(1000):
     if csv_training.shape[1] == 3:
         data_training = [csv_training[0][i], csv_training[1][i]]
         label_training = csv_training[2][i]
@@ -54,25 +55,72 @@ for i in range(20):
 
 
     @qml.qnode(dev)
-    def circuit(datas, params):
+    def entire_circuit(datas, params):
         sensor(datas, wires=[0])
         classifier(params, wires=dev.wires)
         return qml.sample(qml.PauliZ(0))
 
 
-    result = circuit(data_training, weights)
-    prob = result
+    def count_prob(result):
+        prob_qat = 0
+        prob_doq = 0
+        if dev.shots > 1:
+            for i in result:
+                if i == 1:
+                    prob_qat += 1
+                else:
+                    prob_doq += 1
+            if prob_qat > prob_doq:
+                prob = 1
+            elif prob_qat == prob_doq:
+                prob = 0
+            else:
+                prob = -1
+        else:
+            prob = result
+        return prob
+
+
+    @qml.qnode(dev)
+    def optimizer_circuit(params):
+        classifier(params, wires=dev.wires)
+        return qml.expval(qml.PauliZ(0))
+
+
+    def cost(x):
+        return optimizer_circuit(x)
+
+
+    # Run a first time the entire circuit
+    result = entire_circuit(data_training, weights)
+    prob = count_prob(result)
+
+    # Optimize th weights is the output is bad
+    if (prob == -1 and label_training != "doq") or (prob == 1 and label_training != "qat"):
+        steps = 100
+        for i in range(steps):
+            weights = opt.step(cost, weights)
+            result = entire_circuit(data_training, weights)
+            prob = count_prob(result)
+
+            #if (i + 1) % 10 == 0:
+            # print("Cost after step {:5d}: {: .7f} -> {}".format(i + 1, prob, label_training))
+
+            if (prob == -1 and label_training == "doq") or (prob == 1 and label_training == "qat"):
+                break
+
+    # Calculate error rate
+    if (prob == -1 and label_training != "doq") or (prob == 1 and label_training != "qat"):
+        error_rate += 1
 
     # Graph and check error
     prediction[0].append(data_training[0])
     prediction[1].append(data_training[1])
     if csv_training.shape[1] == 3:
         prediction[2].append(prob)
-        print("Data : {} ; {} = {}".format(data_training[0], data_training[1], prob))
     else:
         prediction[2].append(data_training[2])
         prediction[3].append(prob)
-        print("Data : {} ; {} ; {} = {}".format(data_training[0], data_training[1], data_training[1], prob))
 
 print("\nTraining done !")
 
@@ -85,34 +133,61 @@ else:
     fig.scatter(prediction[0], prediction[1], prediction[2], c=prediction[3], cmap="coolwarm")
 plt.show()
 
+print("Error rate : {} %".format(round(error_rate / len(prediction[0]) * 100, 3)))
 
-##################################################################
-# Testing phase
 
-# print("##################################################################\n"
-#       "# TESTING")
-# """
-#     Variables :
-#     data_testing : the xy // xyz data for only 1 qubit
-#     theta : the angle for the classifier (given during the training phase)
-# """
-#
-# for i in range(len(csv_testing[0])):
-#     if csv_testing.shape[1] == 2:
-#         data_testing = [csv_testing[0][i], csv_testing[1][i]]
-#     else:
-#         data_testing = [csv_testing[0][i], csv_testing[1][i], csv_testing[2][i]]
-#
-#     dev = qml.device('default.qubit', wires=1)
-#
-#
-#     @qml.qnode(dev)
-#     def circuit(datas, params):
-#         sensor(datas, wires=[0])
-#         classifier(params, wires=[0])
-#         return qml.expval(qml.PauliZ(0))
-#
-#
-#     result = circuit(data_testing, theta)
-#     prob = result
-#     print(prob)
+print("##################################################################\n"
+      "# Testing")
+"""
+    Variables :
+    data_training : the xy // xyz data for only 1 qubit
+    label_training : the label for only 1 qubit
+    n_layers : deep level
+    weights : the angle for the classifier
+"""
+
+dev = qml.device('default.qubit', wires=1, shots=1)
+
+if csv_testing.shape[1] == 2:
+    prediction = [[], [], []]
+else:
+    prediction = [[], [], [], []]
+
+for i in range(len(csv_testing[0])):
+    # for i in range(20):
+    if csv_testing.shape[1] == 2:
+        data_testing = [csv_testing[0][i], csv_testing[1][i]]
+    else:
+        data_testing = [csv_testing[0][i], csv_testing[1][i], csv_testing[2][i]]
+
+
+    @qml.qnode(dev)
+    def entire_circuit(datas, params):
+        sensor(datas, wires=[0])
+        classifier(params, wires=dev.wires)
+        return qml.sample(qml.PauliZ(0))
+
+
+    # Run the circuit
+    result = entire_circuit(data_testing, weights)
+    prob = result
+
+    # Graph and check error
+    prediction[0].append(data_testing[0])
+    prediction[1].append(data_testing[1])
+    if csv_testing.shape[1] == 2:
+        prediction[2].append(prob)
+    else:
+        prediction[2].append(data_testing[2])
+        prediction[3].append(prob)
+
+print("\nTesting done !")
+
+if len(prediction) == 3:
+    plt.scatter(prediction[1], prediction[0], c=prediction[2], cmap="coolwarm")
+    plt.plot([0,2*np.pi], [np.pi/2, np.pi/2], linestyle='--', c='#000000')
+    plt.colorbar()
+else:
+    fig = plt.figure().gca(projection='3d')
+    fig.scatter(prediction[0], prediction[1], prediction[2], c=prediction[3], cmap="coolwarm")
+plt.show()
